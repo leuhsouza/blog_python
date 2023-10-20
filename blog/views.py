315099666e9dfdabb2b702a1f8840bfae4dbcb09
Leonardo Souza <leuhsouza@gmail.com>
2023-10-20 06:10:11 -0300

    atualizando

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
